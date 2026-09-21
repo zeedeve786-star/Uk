a874d429d42c4b8e16c16a8044ed62cd45c3738c
dev8-droid <devyousaf8@gmail.com>
2026-09-21 07:40:14 -0700

    Remove node_modules directory

--- node_modules/@stripe/stripe-js/dist/pure.d.ts
+++
@@ -1,7 +0,0 @@
-import {loadStripe as _loadStripe} from './shared';
-
-export {ReleaseTrain} from './shared';
-
-export const loadStripe: typeof _loadStripe & {
-  setLoadParameters: (params: {advancedFraudSignals: boolean}) => void;
-};
--- node_modules/@stripe/stripe-js/dist/shared.d.ts
+++
@@ -1,8 +0,0 @@
-import {StripeConstructorOptions, Stripe} from './stripe-js';
-
-export type ReleaseTrain = 'dahlia';
-
-export const loadStripe: (
-  publishableKey: string,
-  options?: StripeConstructorOptions | undefined
-) => Promise<Stripe | null>;
